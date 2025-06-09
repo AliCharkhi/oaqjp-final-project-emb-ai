cc68d48a6dfbeb43b30086dc6e600bcd0bf4ec0a
AliCharkhi <a_ch_2004@yahoo.com>
2025-06-09 12:22:28 -0400

    modify the output for emotion detection module

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,15 +1,26 @@
 import json, requests
 
-def emotion_detector(text_to_analyse):
+def emotion_detector(text_to_analyze):
     
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/\
-    watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     HEADER = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    MYOBJ = { "raw_document": { "text": text_to_analyse } }
+    MYOBJ = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(URL, json=MYOBJ, headers=HEADER)
 
     if response.status_code == 500:
         return {"error": "Server Error"}
 
-    return response.text
+    response_json = json.loads(response.text)
+    emotions = response_json['emotionPredictions'][0]['emotion']
+        
+    results = {
+     'anger': emotions['anger'],
+     'disgust': emotions['disgust'],
+     'fear': emotions['fear'],
+     'joy': emotions['joy'],
+     'sadness': emotions['sadness'],
+     'dominant_emotion': max(emotions, key=emotions.get)
+     }
+
+    return results
